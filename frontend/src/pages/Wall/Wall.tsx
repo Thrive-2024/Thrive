521ac84083e0df2320fb326114f1da8a660d3cf1
nafutofu <naufal.syaqil@gmail.com>
2024-02-26 10:57:06 +0800

    Clean

--- frontend/src/pages/Wall/Wall.tsx
+++ frontend/src/pages/Wall/Wall.tsx
@@ -1,33 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { Navbar, RightNavbar, MidTopSection } from '../../Navbar';
-import { Box, Divider, Grid, Typography } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import { Navbar, RightNavbar, MidTopSection } from "../../Navbar";
+import { Box, Divider, Grid, Typography } from "@mui/material";
 
 export const Wall = () => {
+  useEffect(() => {}, []);
 
-    useEffect(() => {
-    }, []);
-
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', overflow: 'hidden' }}>
-
-            <Box sx={{ display: 'flex', width: '100%' }}>
-                <Box id="navbar">                    <Navbar />
-                </Box>
-                <Box sx={{ width: '80%' }}>
-                    <Box>
-                        <MidTopSection />
-                        <Divider />
-                    </Box>
-                    <Grid container spacing={3} sx={{ padding: 5, pt: 4 }} >
-                        <Grid item xs={12} >
-                            <Typography variant='h5' sx={{ ml: 1 }}>Your Wall</Typography>
-
-                        </Grid>                                {/* Wall content here */}
-                    </Grid></Box>
-                <Box id="rightNavbar" sx={{ boxShadow: '0px 0px 1px rgba(0,0,0,0.5)' }}>
-                    <RightNavbar />
-                </Box>
-            </Box>
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <Box sx={{ display: "flex", width: "100%" }}>
+        <Box id="navbar">
+          {" "}
+          <Navbar />
+        </Box>
+        <Box sx={{ width: "80%" }}>
+          <Box>
+            <MidTopSection />
+            <Divider />
+          </Box>
+          <Grid container spacing={3} sx={{ padding: 5, pt: 4 }}>
+            <Grid item xs={12}>
+              <Typography variant="h5" sx={{ ml: 1 }}>
+                Your Wall
+              </Typography>
+            </Grid>{" "}
+            {/* Wall content here */}
+          </Grid>
+        </Box>
+        <Box id="rightNavbar" sx={{ boxShadow: "0px 0px 1px rgba(0,0,0,0.5)" }}>
+          <RightNavbar />
+        </Box>
+      </Box>
+    </div>
+  );
+};
