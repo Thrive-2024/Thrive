f3167b1c7dfcdd5211b831566350327fbf1c1f72
petrinawjt <petrinawong1909@gmail.com>
2024-02-29 11:27:40 +0800

    [extension] redo layout with tailwind

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,107 +1,27 @@
 import './App.css';
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Tabs from '@mui/joy/Tabs';
-import TabList from '@mui/joy/TabList';
-import Tab, { tabClasses } from '@mui/joy/Tab';
-import TabPanel from '@mui/joy/TabPanel';
-import Typography from '@mui/joy/Typography';
-import AssignmentOutlinedIcon from '@mui/icons-material/AssignmentOutlined';
-import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
-import ReportProblemOutlinedIcon from '@mui/icons-material/ReportProblemOutlined';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Container } from '@mui/joy';
-import './styles/globals.css'
-import Pomodoro from './popup/Pomodoro'
+import React, { useState } from 'react';
+import Pomodoro from './popup/Pomodoro';
 
 function App() {
 
-  const [value, setValue] = React.useState('1');
-
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
-
-
   return (
-    <Tabs
-      className="h-screen w-screen flex justify-center items-center"
-      variant="outlined"
-      aria-label="Pricing plan"
-      defaultValue={0}
-      orientation="vertical"
-      sx={{
-        height: '100%',
-        width: 400,
-        borderRadius: 'lg',
-        boxShadow: 'sm',
-        overflow: 'auto',
-        flexDirection: 'row-reverse'
-      }}
-    >
-      <TabList
-        disableUnderline
-        tabFlex={1}
-        sx={{
-          [`& .${tabClasses.root}`]: {
-            fontSize: 'sm',
-            fontWeight: 'lg',
-            [`&[aria-selected="true"]`]: {
-              color: 'primary.500',
-              bgcolor: 'background.surface',
-            },
-            [`&.${tabClasses.focusVisible}`]: {
-              outlineOffset: '-4px',
-            },
-          },
-        }}
-      >
-        <Tab disableIndicator variant="soft" sx={{ flexGrow: 1 }}>
-          <AssignmentOutlinedIcon sx={{ color:'#99C2F0' }} />
-        </Tab>
-        <Tab disableIndicator variant="soft" sx={{ flexGrow: 1 }}>
-          <AccessTimeOutlinedIcon sx={{ color:'#99C2F0' }} />
-        </Tab>
-        <Tab disableIndicator variant="soft" sx={{ flexGrow: 1 }}>
-          <ReportProblemOutlinedIcon sx={{ color:'#99C2F0' }} />
-        </Tab>
-      </TabList>
+    <div>
+      <head>
+        <script src="https://cdn.tailwind.com"></script>
+      </head>
 
-      {/* Assignment Tab */}
-      <TabPanel value={0}>
-        <Container sx={{ display:'flex' }}>
-          <Typography sx={{ color:"#A3A3A3", marginRight:'10%', fontSize:'14px' }} >
-              Currently working on
-          </Typography>
-          <MoreVertIcon sx={{ fontSize:'20px', marginLeft:'10%' }} />
-        </Container>
-        <Typography level="inherit">
-          Assignment 2
-        </Typography>
-        <Container sx={{ display:'flex' }} >
-          <Typography sx={{ background:'#CCCCFF', borderRadius:'20px', p:0.5 }} >
-            Linear Algebra
-          </Typography>
-          <Typography>
-            2 days left
-          </Typography>
-        </Container>
-      </TabPanel>
-
-      {/* Pomodoro Timer Tab */}
-      <TabPanel value={1}>
-        <Pomodoro />
-      </TabPanel>
-
-      {/* Site Restriction Tab */}
-      <TabPanel value={2}>
-        <Typography level="inherit">
-          Restricted Sites
-        </Typography>
-      </TabPanel>
-
-    </Tabs>
+      <body>
+        <div className="tab-section bg-gray-100 rounded-lg p-5 border-2 border-indigo-200">
+          <div className="flex flex-wrap gap-1">
+            <button className="p-4 rounded-lg text-gray-700 font-bold" data-tab-target="#tab1">Profile</button>
+            <button className="p-4 rounded-lg text-gray-700 font-bold" data-tab-target="#tab1">Profile</button>
+            <button className="p-4 rounded-lg text-gray-700 font-bold" data-tab-target="#tab1">Profile</button>
+          </div>
+        </div>
+      </body>
+    </div>
   );
 }
 
 export default App;
+
