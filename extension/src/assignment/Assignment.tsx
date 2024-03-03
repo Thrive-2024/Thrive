47a02248f249037d96efb98bfa91fb283ac52bb7
swiifttay <126874041+swiifttay@users.noreply.github.com>
2024-03-03 16:36:06 +0800

    [extension - assignment] selected task interaction

--- extension/src/assignment/Assignment.tsx
+++ extension/src/assignment/Assignment.tsx
@@ -10,11 +10,21 @@ import { REACT_APP_BACKEND_DEV_URL } from "../constants/envConsts";
 
 const ITEM_HEIGHT = 48;
 
+type Task = {
+    id: string,
+    taskName: string,
+    subjectName: string,
+    colour: string,
+    dueDate: string,
+    notes: string
+}
+
 const Assignment = () => {
 
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const [options, setOptions] = useState<string[]>([]);
+    const [tasks, setTasks] = useState<Task[]>([])
     const [email, setEmail] = useState<string>('');
+    const [selectedTask, setSelectedTask] = useState<Task>();
 
     async function fetchData(receivedEmail: string) {
         try {
@@ -37,9 +47,24 @@ const Assignment = () => {
                 throw new Error('Invalid data format received from the server');
             }
 
-            const taskNames = tasks.map((item: any) => item.taskName).filter((taskName: string) => taskName);
+            const mappedTasks: Task[] = tasks
+                .filter((task: any) => task.taskStatus !== 'completed')
+                .map((task: any) => ({
+                    id: task?._id,
+                    taskName: task?.taskName,
+                    subjectName: task?.subjectName,
+                    colour: task?.colour,
+                    dueDate: task?.dueDate,
+                    notes: task?.notes
+                }));
 
-            setOptions(taskNames);
+            setTasks(mappedTasks);
+
+            if (mappedTasks.length !== 0) {
+                chrome.storage.sync.set({ selectedTask: mappedTasks[0] });
+                setSelectedTask(mappedTasks[0]);
+            }
+            
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -47,7 +72,7 @@ const Assignment = () => {
 
     useEffect(() => {
         async function fetchDataAndSetEmail() {
-            // Retrieve the current state of the extension (whether it's on or off) from Chrome storage
+            // Retrieve the user email
             const data = await new Promise<{ currentUser: string }>((resolve) => {
                 chrome.storage.sync.get("currentUser", (data) => {
                     resolve({ currentUser: data.currentUser });;
@@ -71,6 +96,27 @@ const Assignment = () => {
     const handleClose = () => {
         setAnchorEl(null);
     };
+
+    const handleMenuItemClick = (index: number) => {
+        setSelectedTask(tasks[index]);
+        chrome.storage.sync.set({ selectedTask: tasks[index] });
+    }
+
+    function daysLeftUntilDate(targetDateStr: string): number {
+        const targetDate = new Date(targetDateStr);
+        const today = new Date();
+        
+        // Set both dates to midnight to compare only the date portion
+        today.setHours(0, 0, 0, 0);
+        targetDate.setHours(0, 0, 0, 0);
+
+        const todayMs = today.getTime();
+        const targetDateMs = targetDate.getTime();
+        const differenceMs = targetDateMs - todayMs;
+        const daysDifference = Math.ceil(differenceMs / (1000 * 60 * 60 * 24));
+
+        return daysDifference;
+    }
 
     return (
         <div style={{ width: '90%', marginLeft: '10px', marginTop: '10px' }}>
@@ -98,14 +144,19 @@ const Assignment = () => {
                             overflowY: 'auto'
                         }}
                     >
-                        {options.length === 0 ? (
+                        {tasks.length === 0 ? (
                             <MenuItem disabled sx={{ fontSize: '14px' }}>
                                 No tasks available
                             </MenuItem>
                         ) : (
-                            options.map((option: string, index: number) => (
-                                <MenuItem key={index} selected={option === 'Pyxis'} onClick={handleClose} sx={{ fontSize: '14px' }}>
-                                    {option}
+                            tasks.map((task: Task, index: number) => (
+                                <MenuItem
+                                    key={index}
+                                    selected={task.id === selectedTask?.id} 
+                                    onClick={() => handleMenuItemClick(index)}
+                                    sx={{ fontSize: '14px' }}
+                                >
+                                    {task.taskName} 
                                 </MenuItem>
                             ))
                         )}
@@ -120,33 +171,39 @@ const Assignment = () => {
                     fontWeight: '500'
                 }}
             >
-                Assignment 2
+                {selectedTask ? selectedTask.taskName : "No Tasks Available"}
             </Typography>
 
             {/* third section: assignment tag and days left */}
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: '8px' }} >
-                <Typography
-                    sx={{
-                        fontSize: '12px',
-                        backgroundColor: '#CCCCFF',
-                        borderRadius: '11px',
-                        paddingTop: '1px',
-                        paddingBottom: '1px',
-                        paddingLeft: '7px',
-                        paddingRight: '7px'
-                    }}
-                >
-                    Linear Algebra
-                </Typography>
-                <Typography
-                    sx={{
-                        color: '#787486',
-                        fontSize: '14px'
-                    }}
-                >
-                    2 days left
-                </Typography>
-            </Box>
+            {selectedTask && (
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: '8px' }} >
+                    <Typography
+                        sx={{
+                            fontSize: '12px',
+                            backgroundColor: `${selectedTask.colour}`,
+                            borderRadius: '11px',
+                            paddingTop: '1px',
+                            paddingBottom: '1px',
+                            paddingLeft: '7px',
+                            paddingRight: '7px'
+                        }}
+                    >
+                        {selectedTask.subjectName}
+                    </Typography>
+                    <Typography
+                        sx={{
+                            color: '#787486',
+                            fontSize: '14px'
+                        }}
+                    >
+                        {daysLeftUntilDate(selectedTask.dueDate) >= 0 ? (
+                            `${daysLeftUntilDate(selectedTask.dueDate)} days left`
+                        ) : (
+                            "Overdue!"
+                        )}
+                    </Typography>
+                </Box>
+            )}
 
             {/* fourth section: notes title*/}
             <Box sx={{ display: 'flex', marginTop: '25px' }}>
@@ -169,7 +226,12 @@ const Assignment = () => {
                         fontSize: '12px'
                     }}
                 >
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. */}
+                    {selectedTask ? (
+                        selectedTask.notes ? selectedTask.notes : "No notes"
+                    ) : (
+                        "Create tasks on Thrive's webpage to view here!"
+                    )}
                 </Typography>
             </Box>
 
