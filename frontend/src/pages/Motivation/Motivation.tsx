dc2e367d5d2bc5cac0dcc0a2080b244edb6ca551
swiifttay <126874041+swiifttay@users.noreply.github.com>
2024-03-18 20:20:35 +0800

    [motivation wall] - wall follows user's msgs

--- frontend/src/pages/Motivation/Motivation.tsx
+++ frontend/src/pages/Motivation/Motivation.tsx
@@ -75,7 +75,7 @@ export const Motivation = (props: any) => {
         const fetchMessages = async () => {
             try {
                 const response = await fetch(
-                    `${process.env.REACT_APP_BACKEND_PRODUCTION_URL}/motivation/getAllByReceiver?receiver=james@gmail.com`
+                    `${process.env.REACT_APP_BACKEND_PRODUCTION_URL}/motivation/getAllByReceiver?receiver=${props.currentUser}`
                 );
                 const input = await response.json();
                 let data = input.data;
